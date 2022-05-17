66bc559596cc15addcf5e17c1c43bf81b1be9471
Aj Raj <akmalraj07@gmail.com>
2022-05-17 16:14:56 +0600

    initial

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import Facebook from "next-auth/providers/facebook";
 
 export default NextAuth({
+    secret:process.env.SECRET,
     providers:[
         Facebook({
             clientId: process.env.FACEBOOK_CLIENT_ID,
